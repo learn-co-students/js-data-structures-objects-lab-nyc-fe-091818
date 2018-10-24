915a14f1fc19f6160b7d3626bd0cd2edcf02e2d9
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-10-24 00:18:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const driverCopy = {...driver}
+  driverCopy.address = "11 Broadway"
+  return driverCopy
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver.address = "12 Broadway"
+  return driver
+}
+function deleteFromDriverByKey(driver, key) {
+  const driverCopy = {...driver}
+  delete driverCopy.name
+  return driverCopy
+}
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver.name
+  return driver 
+}
